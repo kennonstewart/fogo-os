0c430f0a1b959d0133ef0c5f5775554aff93c705
Kennon Stewart <kennon.stewart@gmail.com>
2025-06-16 14:42:16 +0000

    loading wireframe

--- firehouse/main.py
+++ firehouse/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
-from app.api import endpoints
+from api import endpoints
 
 app = FastAPI()
 app.include_router(endpoints.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
