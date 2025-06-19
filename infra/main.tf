2a6bc7990f3759608c4f98a9aa1c875f2c08d15a
Kennon Stewart <kennon.stewart@gmail.com>
2025-06-19 19:13:33 +0000

    loading deepface dependency lambda layer

--- infra/main.tf
+++ infra/main.tf
@@ -50,6 +50,13 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_lambda_layer_version" "deepface_layer" {
+  layer_name          = "deepface-dependencies"
+  compatible_runtimes = ["python3.11"]
+  s3_bucket           = "ssl-865117862950-lambda-layer  "
+  s3_key              = "deepface_layer.zip"
+}
+
 # 4. Lambda Function for facial recognition (placeholder)
 resource "aws_lambda_function" "frame_processor" {
   function_name = "firehouse_frame_processor"
