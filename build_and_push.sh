de0079005080a4df11a3bfa4d1fdd124807ad868
Kennon Stewart <kennon.stewart@gmail.com>
2025-06-19 20:14:04 +0000

    loading

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
 # -------- CONFIGURATION --------
 AWS_REGION="us-east-2"
 ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
